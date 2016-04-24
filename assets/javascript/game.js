afbc8bf48452affe0d0e37590f7ab4e0d694913e
Robert Bethencourt <robert.bethencourt@gmail.com>
2016-04-24 19:06:09 -0400

    attack through to completion of game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -18,28 +18,32 @@ $(document).ready(function(){
 			health_points: this.base_health_points,
 			base_attack_power: 20,
 			attack_power: this.base_attack_power,
-			counter_attack_power: this.base_attack_power
+			counter_attack_power: this.base_attack_power,
+			ch_number: 0
 		}, {
 			name: "Vader",
 			base_health_points: 500,
 			health_points: this.base_health_points,
 			base_attack_power: 50,
 			attack_power: this.base_attack_power,
-			counter_attack_power: this.base_attack_power
+			counter_attack_power: this.base_attack_power,
+			ch_number: 1
 		}, {
 			name: "Ghost Obi-Wan",
 			base_health_points: 999,
 			health_points: this.base_health_points,
 			base_attack_power: 20,
 			attack_power: this.base_attack_power,
-			counter_attack_power: this.base_attack_power
+			counter_attack_power: this.base_attack_power,
+			ch_number: 2
 		}, {
 			name: "Lando",
 			base_health_points: 300,
 			health_points: this.base_health_points,
 			base_attack_power: 20,
 			attack_power: this.base_attack_power,
-			counter_attack_power: this.base_attack_power
+			counter_attack_power: this.base_attack_power,
+			ch_number: 3
 		}],
 		characters_length: 4, // this was coming up undefined as this.characters.length and I'm not sure why
 
@@ -48,7 +52,7 @@ $(document).ready(function(){
 
 			$(this.text_box).html("Choose one of the characters above as your character.");
 			$(this.attack_button).prop('disabled', true);
-			$(this.restart_button).hide();
+			$(this.restart_button).prop('disabled', true);
 
 			// health points
 			// hard coding 4 as that is the max amount of players on screen at one time
@@ -63,6 +67,10 @@ $(document).ready(function(){
 
 				// reseting attack power to base attack power
 				this.characters[i].attack_power = this.characters[i].base_attack_power;
+
+				// remove the .dead-character class from all the images
+				$(this.ch_divs[i]).find("img").removeClass("dead-character");
+				$(this.health_bar).removeClass("progress-bar-warning progress-bar-danger");
 
 			} // end for loop
 
@@ -91,8 +99,8 @@ $(document).ready(function(){
 			this.active_enemies = 0;
 
 			// set the active player and active enemy to empty strings
-			this.active_player = "";
-			this.active_enemy = "";
+			this.active_player = {};
+			this.active_enemy = {};
 		},
 
 		// player choses a character function. This repoisitons the selected character on the gameboard and displays text asking the player to select the first enemy to engage.
@@ -107,7 +115,6 @@ $(document).ready(function(){
 
 					// set the active player to the one selected
 					this.active_player = this.characters[i];
-					console.log(this.active_player);
 				} // end if
 			} // end for loop
 
@@ -137,7 +144,6 @@ $(document).ready(function(){
 
 						// set the active enemy to the one selected
 						this.active_enemy = this.characters[i];
-						console.log(this.active_enemy);
 					} // end if
 				} // end for loop
 
@@ -162,9 +168,72 @@ $(document).ready(function(){
 		},
 
 		attack: function () {
-			
+
+			// Attack Section			
 			// decrease the health points of the attacked enemy
-			this.active_enemy
+			this.active_enemy.health_points = this.active_enemy.health_points - this.active_player.attack_power;
+
+			// increase the attack power of the active player
+			this.active_player.attack_power = this.active_player.attack_power + this.active_player.base_attack_power;
+
+			console.log(this.active_player.base_attack_power + " / " + this.active_player.attack_power);
+
+			// displays to the screen
+			// update the active enemie's health points
+			$(this.health_points_span[this.active_enemy.ch_number]).html(this.active_enemy.health_points);
+
+			// store the percentage of the remaining healthbar in a variable
+			var health_bar_percentage = (this.active_enemy.health_points / this.active_enemy.base_health_points) * 100;
+			// if the health bar is below 50% add the bootstrap warning class to the progress bar
+			if (health_bar_percentage < 50) {
+				$(this.health_bar[this.active_enemy.ch_number]).addClass("progress-bar-warning");
+			} // end if
+			// if the health bar is below 20% add the bootstrap danger class to the progress bar
+			if (health_bar_percentage < 20) {
+				$(this.health_bar[this.active_enemy.ch_number]).addClass("progress-bar-danger");	
+			} // end if
+			// update the width of the progress bar
+			$(this.health_bar[this.active_enemy.ch_number]).css("width", health_bar_percentage + "%");
+
+			// Dead Character Section
+			// if the active enemy's health points are 0 or below (meaning they're toast)
+			if (this.active_enemy.health_points <= 0) {
+
+				// if there are more enemie to fight
+				if (this.selected_characters !== 0) {
+
+					// display the name of the dead character to the screen with
+					$(this.text_box).html("You have killed " + this.active_enemy.name + "<p>Select another character to fight</p>");
+
+				} else {
+
+					// you have won the game so display a congratulatory message
+					$(this.text_box).html("You have killed " + this.active_enemy.name + "<p>Congratulations! You are the ruler of the galaxy.</p>");
+
+					// display the Restart button
+					$(this.restart_button).prop('disabled', false);
+
+				} // end if else
+
+				// set the health points to 0 so no negative numbers show
+				$(this.health_points_span[this.active_enemy.ch_number]).html("0");
+
+				// move the active enemy to the right of the screen and add the .dead-chaaracter class to the image
+				$(this.ch_divs[this.active_enemy.ch_number]).css("right", "0%");
+				$(this.ch_divs[this.active_enemy.ch_number]).find("img").addClass("dead-character");
+
+				// set the active enemy to an empty object and set the active number of enemies to 0
+				this.active_enemy = {};
+				this.active_enemies = 0;
+
+				// activate the attack button
+				$(this.attack_button).prop('disabled', true);
+
+			}
+
+
+			// Counter Attack Section
+			// if active enemy is not an empty object
 		}
 	}
 
@@ -189,6 +258,12 @@ $(document).ready(function(){
 	$(starWarsGame.attack_button).on("click", function (e) {
 
 		starWarsGame.attack();
+
+	});
+
+	$(starWarsGame.restart_button).on("click", function (e) {
+
+		starWarsGame.startGame();
 
 	})
 
